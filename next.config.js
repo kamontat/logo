420b8184bba83fb0921910bfe12b7be5c16d1086
Kamontat Chantrachirathumrong <developer@kamontat.net>
2020-05-24 00:47:15 +0700

    feat(ui): cutom css classname

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
-module.exports = {
+const withCSS = require('@zeit/next-css')
+
+const isDev = process.env.NODE_ENV === "development"
+module.exports = withCSS({
+  poweredByHeader: false,
   env: {
     buildtime: +new Date(),
   },
-}
+  cssModules: true,
+  cssLoaderOptions: {
+    importLoaders: 1,
+    localIdentName: isDev ? "[path][name]__[local]" : "[hash:base64]",
+  }
+})
